de968469161cc776e42a1d0ed82a16037dc57ddb
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2021-12-16 18:24:12 -0700

    Initial Commit

---
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioAppApp.swift
+//  UltimatePortfolioApp
+//
+//  Created by Mitch Andrade on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
