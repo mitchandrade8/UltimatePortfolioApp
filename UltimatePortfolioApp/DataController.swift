311ee4adffea0eee7e9ae0bb3e8407ff77d48bd3
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-02-04 13:02:48 -0700

    Initial  commit.

---
+++ UltimatePortfolioApp/DataController.swift
@@ -0,0 +1,84 @@
+//
+//  DataController.swift
+//  UltimatePortfolioApp
+//
+//  Created by Mitch Andrade on 12/16/21.
+//
+
+import CoreData
+import SwiftUI
+                        // Allows any SwiftUI view to create instances of this,
+class DataController: ObservableObject {
+    let container: NSPersistentCloudKitContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "Main")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                fatalError("Fatal error loading store: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)
+        
+        do {
+            try dataController.createSampleData()
+        } catch {
+            fatalError("Fatal error creating preview: \(error.localizedDescription)")
+        }
+        
+        return dataController
+    }()
+    
+    func createSampleData() throws {
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            let project = Project(context: viewContext)
+            project.title = "Project \(i)"
+            project.items = []
+            project.creationDate = Date()
+            project.closed = Bool.random()
+            
+            for j in 1...10 {
+                let item = Item(context: viewContext)
+                item.title = "Item \(j)"
+                item.creationDate = Date()
+                item.completed = Bool.random()
+                item.project = project
+                item.priority = Int16.random(in: 1...3)
+            }
+        }
+        
+        try viewContext.save()
+    }
+    
+    func save() {
+        if container.viewContext.hasChanges {
+            try? container.viewContext.save()
+        }
+    }
+    
+    func delete(_ object: NSManagedObject) {
+        container.viewContext.delete(object)
+    }
+    
+    func deleteAll() {
+        let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Item.fetchRequest()
+        
+        let batchDeleteRequest1 = NSBatchDeleteRequest(fetchRequest: fetchRequest1)
+        _ = try? container.viewContext.execute(batchDeleteRequest1)
+        
+        let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Item.fetchRequest()
+        
+        let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2)
+        _ = try? container.viewContext.execute(batchDeleteRequest2)
+    }
+}
---
+++ UltimatePortfolioApp/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  UltimatePortfolioApp
+//
+//  Created by Mitch Andrade on 2/3/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
